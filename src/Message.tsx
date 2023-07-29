07c1deac7a7c09b89fdd56fae5e4b479ceb3686c
Willian Belolli <williamcharllesbelolli@hotmail.com>
2023-07-29 01:43:26 +0100

    creating cool apps with React

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+//PascalCasing
+function Message() {
+ //JSX - JavaScript XML
+ const firstName = 'Willian';  
+ if (firstName) 
+ return <h1>Hello {firstName}</h1>;
+ return <h1>Hello World!!</h1>;
+}
+
+export default Message;
